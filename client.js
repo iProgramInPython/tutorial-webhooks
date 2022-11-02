98c45f629b4988a9d1b0b0a5721fc3d948348378
iProgramInPython <105540155+iProgramInPython@users.noreply.github.com>
2022-11-02 05:36:19 +0300

    Update client.js

--- client.js
+++ client.js
@@ -6,4 +6,4 @@ const Discord = require('discord.js');
 
 const hook = new Discord.WebhookClient('webhook id', 'webhook token');
 
-hook.send('Server Status: DOWN! <a:gostatus:1032519464396263424>');
+hook.send('text');
